96b0a79f010ca3f29002930b53a7ed2f2abbc482
Jacob <millerjr@tcd.ie>
2025-12-05 15:51:48 -0300

    updated config to deal with bad urls

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        notFound: "404.html",
+      },
+    },
+  },
   plugins: [react()],
-})
+});
