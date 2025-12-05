67dd63397d9e1dc4006aecddbaf72a787e57972e
Jacob <millerjr@tcd.ie>
2025-12-05 15:55:00 -0300

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        notFound: "404.html",
-      },
-    },
-  },
   plugins: [react()],
 });
